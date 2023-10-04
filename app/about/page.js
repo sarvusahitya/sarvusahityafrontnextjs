dbcd961732642d4380cd437e8e2b06d2841f0250
sarvusahitya <sarvusahitya@gmail.com>
2023-10-04 21:43:56 +0530

    update

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,10 @@
 // pages/index.js
 import React from "react";
-import PostList from "../components/PostList";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Homepage</h1>
-      {/* <PostList posts={posts} /> */}
       {/* You can make additional API calls here */}
     </div>
   );
