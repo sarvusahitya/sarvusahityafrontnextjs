b69a2ac2e459a8d3a055a95fa66dcb799decc732
sarvusahitya <sarvusahitya@gmail.com>
2023-10-05 08:58:54 +0530

    update

--- app/components/ImageSlider.js
+++ app/components/ImageSlider.js
@@ -1,78 +1,78 @@
 // Components/Gallery.js
 "use client";
-import React, { useState } from "react";
-import { AiOutlineExpandAlt } from "react-icons/ai";
-import Lightbox from "yet-another-react-lightbox";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
-import "yet-another-react-lightbox/styles.css";
+import { useEffect, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
 
 const Gallery = () => {
   const [open, setOpen] = useState(false);
   const [image, setImage] = useState("");
 
-  const galleryTab = [
-    // you can add more image if you want
-    {
-      imageUrl:
-        "https://codetraction.com/fileupload/uploads/slider/20230918_094917.png",
-      type: "Nature",
-      title: "Beautiful Work",
-    },
-    {
-      imageUrl:
-        "https://codetraction.com/fileupload/uploads/slider/20230903_212014.png",
-      type: "Nature",
-      title: "Beautiful Work",
-    },
-    {
-      imageUrl:
-        "https://codetraction.com/fileupload/uploads/slider/2023-09-03_22-06-37.jpg",
-      type: "Nature",
-      title: "Beautiful Work",
-    },
-  ];
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const [sliders, setSliders] = useState([]);
+  useEffect(() => {
+    // Define your API endpoint URL
+    const apiUrl = "https://sarvu-sahitya-qdly.onrender.com/sliders";
+
+    // Define the request body
+    const reqBody = {
+      page: 1,
+      size: 10,
+      orderby: -1,
+      orderbycolumnname: "createdAt",
+    };
+
+    // Make a POST request to the API
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqBody),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          setSliders(data.data);
+        } else {
+          console.error("Failed to fetch sliders");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching sliders:", error);
+      });
+  }, []);
+
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
-  const slides = galleryTab.map((item) => ({
-    src: item.imageUrl,
+  const slides = sliders.map((slider) => ({
+    src: slider.slider_media_url,
     width: 3840,
     height: 2560,
   }));
 
   return (
-    <div className="w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {galleryTab.map((x, index) => {
+    <div className="swiper-container">
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+      >
+        {sliders.map((x, index) => {
           return (
-            <div
-              key={index}
-              className="group cursor-pointer relative"
-              onClick={() => openLightbox(index)}
-            >
-              <div
-                className="bg-cover bg-center h-80 md:h-96 lg:h-80 w-full bg-no-repeat"
-                style={{ backgroundImage: `url("${x.imageUrl}")` }}
-              >
-                <div className="text-3xl text-white absolute bottom-0 left-2 z-10">
-                  <div>{x.type}</div>
-                  <div>{x.title}</div>
-                </div>
-              </div>
-              <div className="bg-black opacity-0 group-hover:opacity-75 absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out">
-                <AiOutlineExpandAlt className="text-5xl border w-16 h-16 bg-neutral-500 hover:bg-white hover:text-black p-3 cursor-pointer rounded-full" />
-              </div>
-            </div>
+            <SwiperSlide key={x._id}>
+              <img src={x.slider_media_url} alt="Slide 1" />
+            </SwiperSlide>
           );
         })}
-      </div>
-      <Lightbox
-        open={open}
-        initialSlide={selectedImageIndex}
-        close={() => setOpen(false)}
-        plugins={[Zoom]}
-        showPrevNext={true}
-        slides={slides}
-      />
+      </Swiper>
+      <div className="swiper-button-next"></div>
     </div>
   );
 };
