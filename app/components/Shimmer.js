3c71bf5cc85da1a0587f616edf9493ffcbbff301
sarvusahitya <sarvusahitya@gmail.com>
2023-10-15 22:08:28 +0530

    update

---
+++ app/components/Shimmer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Shimmer.module.css";
+
+const Shimmer = () => {
+  return (
+    <div className="shimmer-loader w-100 h-100">
+      <div className="shimmer"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
-import ImageSlider from "../app/components/ImageSlider";
-import PoetSection from "../app/components/PoetSection";
-import CategorySection from "../app/components/CategorySection";
-import PostSection from "../app/components/PostSection";
+// import ImageSlider from "../app/components/ImageSlider";
+// import PoetSection from "../app/components/PoetSection";
+// import CategorySection from "../app/components/CategorySection";
+// import PostSection from "../app/components/PostSection";
+import dynamic from "next/dynamic";
+const ImageSlider = dynamic(() => import("../app/components/ImageSlider"));
+const PoetSection = dynamic(() => import("../app/components/PoetSection"));
+const CategorySection = dynamic(
+  () => import("../app/components/CategorySection")
+);
+const PostSection = dynamic(() => import("../app/components/PostSection"));
+
 export default function Home() {
   return (
     <section>
