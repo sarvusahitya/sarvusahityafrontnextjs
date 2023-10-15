769d0f1143d9b9d42b77a85931f4bc1608451725
sarvusahitya <sarvusahitya@gmail.com>
2023-10-15 12:54:04 +0530

    update

--- app/components/CategorySection.js
+++ app/components/CategorySection.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import ProfilePicture from "./ProfilePicture";
 const CategorySection = () => {
   // data/users.js
-  const [poets, setPoets] = useState([]);
+  const [category, setCategorys] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
     // Define your API endpoint URL
@@ -29,7 +30,7 @@ const CategorySection = () => {
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
-          setPoets(data.data);
+          setCategorys(data.data);
         } else {
           console.error("Failed to fetch sliders");
         }
@@ -44,7 +45,7 @@ const CategorySection = () => {
       <h2 className="text-2xl font-bold mb-4">Category Section</h2>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
-        {poets.map((user, index) => (
+        {category.map((user, index) => (
           <div key={index} className="text-center">
             <ProfilePicture
               src={user.category_media_url}
