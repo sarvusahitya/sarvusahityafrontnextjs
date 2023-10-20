a6d9c4ac354749369179df228a9a4421bbce35e6
sarvusahitya <sarvusahitya@gmail.com>
2023-10-20 08:43:57 +0530

    update

--- app/components/PostSection.js
+++ app/components/PostSection.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import ProfilePicture from "./ProfilePicture";
 import Link from "next/link";
+import Image from "next/image";
 const PostSection = ({ posts }) => {
   // data/users.js
 
@@ -10,7 +11,13 @@ const PostSection = ({ posts }) => {
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3">
       {posts.map((post, index) => (
         <div className="bg-white rounded-lg shadow-md p-4" key={index}>
-          <img src={post.post_media_url[0]} alt={post.post_name} className="" />
+          <Image
+            src={post.post_media_url[0]}
+            alt={post.post_name}
+            className=""
+            width={1200}
+            height={400}
+          />
           <div className="mt-4">
             <h2 className="text-xl font-semibold">{post.post_name}</h2>
             <p className="text-gray-500">{post.createdAt}</p>
