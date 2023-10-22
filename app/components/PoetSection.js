692fb0454184358b5ae8df2d5d22e4cc629e5cc2
sarvusahitya <sarvusahitya@gmail.com>
2023-10-22 07:38:17 +0530

    update

--- app/components/PoetSection.js
+++ app/components/PoetSection.js
@@ -2,6 +2,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import ProfilePicture from "./ProfilePicture";
+import Link from "next/link";
 const PoetSection = ({ poets }) => {
   return (
     <>
@@ -10,10 +11,12 @@ const PoetSection = ({ poets }) => {
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
         {poets.map((user, index) => (
           <div key={index} className="text-center">
-            <ProfilePicture
-              src={user.profile_media_url}
-              name={user.poet_name}
-            ></ProfilePicture>
+            <Link href={`/poets/${user._id}`} key={index}>
+              <ProfilePicture
+                src={user.profile_media_url}
+                name={user.poet_name}
+              ></ProfilePicture>
+            </Link>
           </div>
         ))}
       </div>
--- app/poets/[poet_slug]/page.js
+++ app/poets/[poet_slug]/page.js
@@ -6,6 +6,9 @@ import React from "react";
 import ReactHtmlParser from "html-react-parser";
 import Image from "next/image";
 import ProfilePicture from "@/app/components/ProfilePicture";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
 import Link from "next/link";
 const PoetSection = () => {
   // data/users.js
@@ -50,51 +53,89 @@ const PoetSection = () => {
   };
 
   return (
-    <>
-      <div className="bg-white p-4 rounded shadow-md max-w-xl mx-auto mt-6">
-        {poets.map((poet, index) => (
+    <div className="bg-white p-4 rounded shadow-md max-w-xl mx-auto mt-6">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : poets.length === 0 ? (
+        <p>No poet data available.</p>
+      ) : (
+        poets.map((poet, index) => (
           <div key={index} className="text-center">
             <ProfilePicture
-              src={poet.profile_media_url}
-              name={poet.poet_name}
+              src={poet.profile_media_url || ""}
+              name={poet.poet_name || "Unknown Poet"}
             ></ProfilePicture>
-            <h1 className="text-2xl font-bold text-center">{poet.poet_name}</h1>
+            <h1 className="text-2xl font-bold text-center">
+              {poet.poet_name || "Unknown Poet"}{" "}
+              {poet.poet_nickname ? `(${poet.poet_nickname})` : ""}
+              {poet.is_blue_tick && (
+                <span className="text-blue-500 ml-2">
+                  <FontAwesomeIcon icon={faCheckCircle} />
+                </span>
+              )}
+            </h1>
+
             <p>
-              <strong>Nickname:</strong> {poet.poet_nickname}
+              <strong>Birthdate:</strong> {poet.poet_birthdate || ""}
             </p>
             <p>
-              <strong>Birthdate:</strong> {poet.poet_birthdate}
+              <strong>Birthplace:</strong> {poet.poet_birthplace || ""}
             </p>
             <p>
-              <strong>Birthplace:</strong> {poet.poet_birthplace}
-            </p>
-            <p>
-              <strong>Home Town:</strong> {poet.poet_home_town}
+              <strong>Home Town:</strong> {poet.poet_home_town || ""}
             </p>
             <p>
               <strong>School Name:</strong>{" "}
-              {ReactHtmlParser(poet.poet_school_name)}
+              {poet.poet_school_name
+                ? ReactHtmlParser(poet.poet_school_name)
+                : ""}
             </p>
             <p>
-              <strong>Address:</strong> {ReactHtmlParser(poet.poet_address)}
+              <strong>Address:</strong>{" "}
+              {poet.poet_address ? ReactHtmlParser(poet.poet_address) : ""}
             </p>
             <p>
               <strong>Languages Known:</strong>{" "}
-              {poet.poet_languages_known.join(", ")}
+              {poet.poet_languages_known && poet.poet_languages_known.length
+                ? poet.poet_languages_known.join(", ")
+                : ""}
             </p>
             <p>
-              <strong>Occupations:</strong> {poet.poet_occupations.join(", ")}
+              <strong>Occupations:</strong>{" "}
+              {poet.poet_occupations && poet.poet_occupations.length
+                ? poet.poet_occupations.join(", ")
+                : ""}
             </p>
             <p>
-              <strong>Hobbies:</strong> {poet.poet_hobbies.join(", ")}
+              <strong>Hobbies:</strong>{" "}
+              {poet.poet_hobbies && poet.poet_hobbies.length
+                ? poet.poet_hobbies.join(", ")
+                : ""}
+            </p>
+            <p>
+              <strong>Nationality:</strong>{" "}
+              {poet.poet_nationality && poet.poet_nationality.length
+                ? poet.poet_nationality.join(", ")
+                : ""}
+            </p>
+            <p>
+              <strong>Awards:</strong>{" "}
+              {poet.awards && poet.awards.length ? poet.awards.join(", ") : ""}
             </p>
             <p className="html-content">
-              <strong>Books:</strong> {ReactHtmlParser(poet.poet_books)}
+              <strong>Books:</strong>{" "}
+              {poet.poet_books ? ReactHtmlParser(poet.poet_books) : ""}
+            </p>
+            <p className="html-content">
+              <strong>Future Books:</strong>{" "}
+              {poet.poet_future_books
+                ? ReactHtmlParser(poet.poet_future_books)
+                : ""}
             </p>
           </div>
-        ))}
-      </div>
-    </>
+        ))
+      )}
+    </div>
   );
 };
 
